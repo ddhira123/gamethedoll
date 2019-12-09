e74f7962352f0f0b54db8de6bd4884ba4a32a814
Dhana <dhana_dhira@apps.ipb.ac.id>
2019-12-09 14:37:40 +0700

    Added gitignore

---
+++ Library/Collab/Download/Assets/Script/EnemyPool.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPool : MonoBehaviour
+{
+    public int humanPoolSize = 5;
+    public GameObject humanPrefab;
+    public GameObject opt1;
+    public GameObject opt2;
+
+    public float spawnRate = 4f;
+    public float humanMin = -1f;
+    public float humanMax = 3.5f;
+
+    public GameObject[] humans;
+    private Vector2 objectPoolPosition = new Vector2(-15f, -25f);
+    private float timeSinceLastSpawn;
+    private float spawnXPos = 30f;
+    private int currentObj = 0;
+    private int rng;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        humans = new GameObject[humanPoolSize];
+        for(int i=0; i < humanPoolSize; i++){
+            rng = Random.Range(0,i);
+            if(rng % 2 == 0){
+                humanPrefab = opt1;
+                objectPoolPosition = new Vector2(-15f, -25f);
+            }
+            else
+            {
+                humanPrefab = opt2;
+                objectPoolPosition = new Vector2(-15f, 0.5f);
+            }
+            humans[i] = (GameObject)Instantiate(humanPrefab, objectPoolPosition, Quaternion.identity);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeSinceLastSpawn += Time.deltaTime;
+
+        if(GameControl.instance.gameOver == false && timeSinceLastSpawn >= spawnRate)
+        {
+            timeSinceLastSpawn = 0f;
+            humans[currentObj].transform.position = new Vector2(spawnXPos, 0f);
+            currentObj++;
+
+            if(currentObj >= humanPoolSize)
+            {
+                currentObj = 0;
+            }
+        }
+    }
+}
---
+++ Library/Collab/Download/Assets/Script/Girl.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Girl : MonoBehaviour
+{
+    public float upForce;
+    private Animator anim;
+    private bool isDead = false;            //Has the player collided with a wall?
+    private Rigidbody2D rb2d;                //Holds a reference to the Rigidbody2D component of the bird.
+    public bool slash = false;
+    public float fallMultiplier = 2.5f;
+
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+		
+    }
+
+    void Update()
+    {
+        //Don't allow control if the bird has died.
+        if (isDead == false)
+        {   
+            rb2d.velocity = new Vector2(0, rb2d.velocity.y);
+            if (Input.GetMouseButtonDown(0))
+            {
+                anim.SetTrigger("Girl_Jump");
+                rb2d.velocity = (new Vector2(rb2d.velocity.x, upForce));
+            }
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+				slash = true;
+				anim.SetTrigger("Girl_Slashing");
+			}
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D trig)
+    {       
+        if(trig.gameObject.CompareTag("human"))
+        {
+            if(slash == true){
+                Destroy(trig.gameObject);
+                GameControl.instance.GirlScored();
+                slash = false;
+            }
+            else {
+                rb2d.velocity = Vector2.zero;
+                isDead = true;
+                anim.SetTrigger("Girl_Die");
+                GameControl.instance.GirlDied();
+            }
+        }
+    }
+}
